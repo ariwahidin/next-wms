9e35dbc3d3db71cd36fe7cf61ea7e99fa8603428
ari.wahidin <ariwahidin21@gmail.com>
2026-04-12 12:02:00 +0700

    fix : fix og-image 03

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <meta property="og:title" content="Yutrack WMS" />
+        <meta property="og:description" content="Integrated Warehouse Management System" />
+        <meta property="og:image" content="https://wms.logspeedy.com/og-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://wms.logspeedy.com" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
